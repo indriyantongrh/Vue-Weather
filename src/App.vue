afe2a1339237e7aa674aec0d83b73058671d8dcd
indriyanto Nugroho <indriyantongrhh@gmail.com>
2021-04-29 13:21:32 +0700

    Initial Commit Weather Vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-           <div class="date">{{ date() }}</div>
+           <div class="date">{{ date()}}</div>
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°C</div>
